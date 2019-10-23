ffeed9bfb6a77de0e6505bbda8028240e2dfc7d9
Owen Wagner <ocwagner@ncsu.edu>
2019-10-23 06:06:49 -0400

    HW_8 submissions for ow

---
+++ battlefield_visits_HW.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(dplyr)
+library(ggthemes)
+library(viridis)
+library(colorRamps)
+library(RColorBrewer)
+library(ggrepel)
+
+###Read in data
+
+park_data<-read_csv('data/2019/2019-09-17/national_parks.csv')
+head(park_data)
+
+military_sites <- c("National Battlefield", "National Battlefield Park", "National Military Park")
+
+###Filter by military sites and calc percent visits
+
+filter(park_data,unit_type %in% military_sites, year==2016)%>%
+  mutate("share_visits"=visitors/sum(visitors))->percent_visits
+
+###assign a state rank
+
+"state_level"<-select(percent_visits, c("state", "visitors", "share_visits"))
+state_level%>%
+  group_by(state)%>%
+  summarize(share_visits=sum(share_visits))%>%
+  mutate("state_rank"=rank(-share_visits))->ranked_states
+View(ranked_states)
+
+###create a factor based on ranking
+
+ranked_states$state <- factor(ranked_states$state, levels = ranked_states$state[order(-ranked_states$state_rank)])
+ranked_states$state
+
+###create custome color palette
+
+mycolors<-colorRampPalette(brewer.pal(8,"Set1"))(14)
+
+### plot bar chart
+
+ggplot(ranked_states, aes(x="", y=share_visits, fill=state))+
+  geom_bar(width = 1, stat ="identity")+
+  geom_text(data = filter(ranked_states, share_visits*100 > 2), aes(x = 1.6, label = paste0(round(share_visits*100), "%")), 
+            position = position_stack(vjust = 0.5)) +
+  scale_fill_manual(values=mycolors,guide = guide_legend(reverse=TRUE))+
+  coord_polar("y")+
+  ggtitle("Share of visits, by state, of battlefields managed by NPS")+
+  xlab("")+
+  ylab("")+
+  theme_tufte()+
+  theme(axis.text=element_blank())+
+  labs(caption = 
+         "percentages not depicted for states comprising less than 2% of total")
+
+
---
+++ park_visits_gas_HW.R
@@ -0,0 +1,85 @@
+library(tidyverse)
+library(dplyr)
+library(ggthemes)
+library(viridis)
+library(colorRamps)
+library(RColorBrewer)
+library(ggrepel)
+
+
+###Load in data
+
+park_data<-read_csv('data/2019/2019-09-17/national_parks.csv')
+head(park_data)
+
+gas_price<-read_csv('data/2019/2019-09-17/gas_price.csv')
+head(gas_price)
+
+population<-read_csv('data/2019/2019-09-17/state_pop.csv')
+View(population)
+
+### US population
+
+population%>%
+  group_by(year)%>%
+  summarize(pop=sum(pop,na.rm=TRUE))->US_pop
+View(US_pop)
+
+### calculate regional totals by year
+
+park_data[c("year","region","visitors")]%>%
+  group_by(region,year)%>%
+  summarize(visitors=sum(visitors))%>%
+  subset(year!="Total")->regional_total
+regional_total
+
+#spread regional totals
+
+regional_total%>%spread(key=region, value=visitors)->regional_spread
+
+#calculate US totals
+
+regional_total%>%
+  group_by(year)%>%
+  summarize(visitors=sum(visitors))%>%
+  rename(US = visitors)->US_total
+
+###join US totals to US regions
+
+left_join(US_total, regional_spread, by="year")%>%
+  gather("US","AK","IM","MW","NC","NE","NT","PW","SE", key="region", value="visitors")%>%
+  transform(year=as.numeric(year))->tidy_region
+
+###join above to gas prices
+
+full_join(tidy_region,gas_price,by="year",-gas_current)->"gas_and_visits"
+
+### join above to US population
+
+full_join(gas_and_visits,US_pop,by="year")%>%
+  mutate("visit_per_cap"=visitors/pop)->"gas_visits_pop"
+
+###add decade column
+
+gas_visits_pop%>%
+mutate("decade"=paste(floor(year/10)*10,'s',sep=""))->"visits_by_decade"
+
+###subset US data
+
+subset(visits_by_decade,region=="US"& year>1930)->subset_US
+View(subset_US)
+
+mycolors<-colorRampPalette(brewer.pal(9,"Set1"))(9)
+
+###draw scatterplot
+
+ggplot(data=subset_US,(aes(gas_constant,visit_per_cap, size=10)))+
+         geom_point(aes(color=decade))+
+  scale_color_brewer(palette="Set1")+
+  guides(size="none")+
+  xlab("gas price - inflation adjusted dollars")+
+  ylab("US park visits per cap")+
+  theme_clean()
+   
+
+
